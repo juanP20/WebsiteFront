c0d0241888e226ee35bf3d452527c46c94735c87
Juan pablo <arenasdurango1999@gmail.com>
2024-09-17 16:24:55 -0500

    The navbar was implemented in the product section, and adjustments were made in products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { SuggestedProductsComponent } from './suggested-products/suggested-products.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    SuggestedProductsComponent
+    SuggestedProductsComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
